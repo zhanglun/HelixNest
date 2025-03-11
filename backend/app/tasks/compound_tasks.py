2c99539245fa71675d18a90f8599457b31b38291
zhanglun <zhanglun1410@gmail.com>
2025-03-11 21:05:45 +0800

    fix: save analysis data in mongodb

--- backend/app/tasks/compound_tasks.py
+++ backend/app/tasks/compound_tasks.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import requests
+from pymongo.collection import ReturnDocument
 from bson.objectid import ObjectId
 from celery import chain, group, shared_task
 from .pubchem_client import fetch_pubchem_data
@@ -16,11 +18,12 @@ def fetch_and_analyze_compound(self, cid):
     fetch_pubchem_data.s(cid)
     | process_raw_data.s()
     | store_to_mongodb.s()
+    | post_storage_analysis.s()
   )
 
   return pipeline()
 
-@shared_task()
+@shared_task(bind=True)
 def process_raw_data(self, raw_data):
   # doc = {
   #   "pubchem_cid": raw_data["pubchem_cid"],
@@ -34,24 +37,74 @@ def process_raw_data(self, raw_data):
 
   return raw_data
 
-@shared_task()
+@shared_task(bind=True)
 def store_to_mongodb(self, data):
+  """
+  Store compound data in MongoDB.
+
+  This task stores the provided compound data into the MongoDB collection. It logs the data being stored and updates the task's progress. After the data is inserted, the inserted document's ID is returned as a string.
+
+  Args:
+    data (dict): The compound data to be stored in the database.
+
+  Returns:
+    str: The ID of the inserted document as a string.
+  """
+
   print("data", data)
   cm = CompoundModel();
-  doc_id = cm.collection.insert_one(data).inserted_id
+  # doc_id = cm.collection.insert_one(data).inserted_id
+  if data.get("created_at") is None:
+    data["created_at"] = datetime.now()
+
+  data['updated_at'] = datetime.now()
+
+  result = cm.collection.find_one_and_update(
+    {"pubchem_cid": data["pubchem_cid"]},
+    {"$set":
+      {
+        "pubchem_cid": data["pubchem_cid"],
+        "iupac_name": data["iupac_name"],
+        "molecular_formula": data["molecular_formula"],
+        "molecular_weight": data["molecular_weight"],
+        "canonical_smiles": data["canonical_smiles"],
+        "isomeric_smiles": data["isomeric_smiles"],
+        "other_smiles": data["other_smiles"],
+        "created_at": data["created_at"],
+        "updated_at": data["updated_at"]
+      }
+    },
+    upsert=True,
+    return_document=ReturnDocument.AFTER
+  )
+
+  print("raw_result", result["_id"])
 
   self.update_state(state='PROGRESS', meta={'progress': 60})
 
-  return str(doc_id)
+  return str(result["_id"])
 
 
-
-@shared_task()
+@shared_task(bind=True)
 def post_storage_analysis(self, doc_id):
+  """
+  Perform analysis on a compound document after it has been stored in the database.
+
+  Given a document ID, this task will fetch the document from the database, extract the SMILES string from the document, and then use the SMILES string to calculate molecular descriptors. The descriptors will then be stored in the database in the same document.
+
+  The task will update the progress of the task to 80 before starting the analysis and to 100 after the analysis is finished.
+
+  Args:
+    doc_id (str): The document ID of the compound to be analyzed.
+
+  Returns:
+    None
+  """
   cm = CompoundModel()
-  doc = cm.collection.find({"_id": ObjectId(doc_id)})
+  doc = cm.collection.find_one({"_id": ObjectId(doc_id)})
 
   self.update_state(state='PROGRESS', meta={'progress': 80})
+
   analysis_result = calculate_descriptors(doc['canonical_smiles'])
 
   cm.collection.update_one(
